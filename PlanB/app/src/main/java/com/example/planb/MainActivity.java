82ec742669e0f9fc4e653216fda09fa5fa0e7e6f
김승규 <rlatmdrb1996@gmail.com>
2019-11-21 21:04:37 +0900

    UI half finish + login update...

--- PlanB/app/src/main/java/com/example/planb/MainActivity.java
+++ PlanB/app/src/main/java/com/example/planb/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.planb;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.guide_select);
+        setContentView(R.layout.regist);
     }
 }

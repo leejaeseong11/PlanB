a1acb21957a711241b032cbdbafa69bf82ea5d24
김승규 <rlatmdrb1996@gmail.com>
2019-11-26 17:26:55 +0900

    오류수정

--- PlanB/app/src/main/java/com/example/planb/destination_regist.java
+++ PlanB/app/src/main/java/com/example/planb/destination_regist.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
